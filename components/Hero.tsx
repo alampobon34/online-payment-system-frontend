7873acf2ad240cb45251674930a04aeb28f37baa
alampobon34 <alampobon34@gmail.com>
2023-10-13 11:39:56 +0600

    changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,8 +29,8 @@ const Hero = () => {
             </div>
           </div>
         </div>
-        <div data-aos-duration="2000" className="w-full md:w-1/2">
-          <Image data-aos="fade-left"
+        <div className="w-full md:w-1/2">
+          <Image data-aos="fade-left" data-aos-duration="3000"
             className="w-full"
             src="/images/hero.png"
             width={200}
